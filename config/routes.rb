1c3d8e8d7bc95c9145376b060bac28741a039001
Frank <t0970612806>
2023-03-21 20:35:55 +0800

    add resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "resumes", to: "resumes#index"
+  get "resumes/new", to: "resumes#new"
+
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
 end
